684e59e19b8e4b488d4af018a471ca5285e447bf
Micah Hawkins <micahbhawkins@gmail.com>
2018-12-04 07:50:34 -0800

    working on day 3

--- 2018/day3/magic-cloth.lisp
+++ 2018/day3/magic-cloth.lisp
@@ -1,1 +1,17 @@
-;; slice it up
+(defparameter *data* (make-array (* 1000 1000) :initial-element 0))
+
+(defun split-by-space (str)
+  "Returns a list of substrings of str"
+  (loop for i = 0 then (1+ j)
+        as j = (position #\Space str :start i)
+        collect (subseq str i j)
+        while j))
+
+(defun parse-rect (str)
+  "parse a rectacngle from a string like #218 @ 278,517: 18x19"
+  (split-by-space str))
+
+(with-open-file (input "input.txt")
+  (when input
+    (loop for line = (read-line input nil)
+          while line do (parse-rect line))))
