e5084672546c8bcc0b9e8963a48130dfb3020467
micah <micahbhawkins@gmail.com>
2018-12-03 16:05:03 -0800

    solves day 2, starting on day 3

--- 2018/day2/box-ids.lisp
+++ 2018/day2/box-ids.lisp
@@ -1,5 +1,5 @@
 (defparameter *data* '())
-(defparameter *counts* (make-hash-table :test 'equal))
+(defparameter *counts* (cons 0 0))
 
 (with-open-file (input "input.txt")
   (when input
@@ -9,16 +9,43 @@
 
 (format t "~a~%" (car *data*))
 
+(defun has (x data)
+  (loop for v being the hash-values of data do
+    (if (= x v) (return t))))
+
 (defun checksum (data)
   ;; process all data
   (loop for id in data do
     ;; process each id
-    (loop for c across id do
-      (if (gethash c *counts*)
-          (incf (gethash c *counts*))
-          (setf (gethash c *counts*) 1)))))
+    (let ((counts (make-hash-table :test 'equal)))
+      (loop for c across id do
+        (if (gethash c counts)
+            (incf (gethash c counts))
+            (setf (gethash c counts) 1)))
+      (if (has 2 counts) (incf (first *counts*)))
+      (if (has 3 counts) (incf (rest *counts*)))))
+  ;; display the results of our checksum
+  (format t
+          "The answer for part 1 is: ~a~%"
+          (* (first *counts*) (rest *counts*))))
 
-(defun show-counts ()
-  (loop for k being the hash-keys in *counts*
-          using (hash-value v) do
-            (format t "~a : ~a~%" k v)))
+(defun diff (a b)
+  (loop for x across a
+        for y across b
+        if (char/= x y) count it))
+
+(defun common (a b)
+  (loop for x across a
+        for y across b
+        if (char= x y) collect x))
+
+(defun find-ids (data)
+  (loop named outer for x in data do
+    (loop for y in data do
+      (if (= 1 (diff x y))
+          (return-from outer (coerce (common x y) 'string))))))
+
+;; (defun show-counts ()
+;;   (loop for k being the hash-keys in *counts*
+;;           using (hash-value v) do
+;;             (format t "~a : ~a~%" k v)))
