24d7d835d2e9f745a05aed1e4c1107e668a45a6f
micah <micahbhawkins@gmail.com>
2018-12-05 20:26:44 -0800

    completes day 3

--- 2018/day3/magic-cloth.lisp
+++ 2018/day3/magic-cloth.lisp
@@ -1,4 +1,7 @@
-(defparameter *data* (make-array (* 1000 1000) :initial-element 0))
+(defparameter *height* 1100)
+(defparameter *width* 1100)
+(defparameter *fabric* (make-array (* *height* *width*) :initial-element 0))
+(defparameter *rects* ())
 
 (defun split (str chr)
   "Returns a list of substrings of str split by chr"
@@ -14,20 +17,43 @@
 (defun plot (xy wh)
   (let ((w (first wh))
         (h (second wh)))
-    (loop for y from (second xy) to (+ (second xy) h) do
-      (loop for x from (first xy) to (+ (first xy) w) do
-        (let ((i (+ x (* y w))))
-          (incf (elt *data* i)))))))
+    (loop for y from (second xy) to (+ (second xy) h -1) do
+      (let ((iy (* y *width*)))
+        (loop for x from (first xy) to (+ (first xy) w -1) do
+          (incf (elt *fabric* (+ x iy))))))))
 
-(defun parse-rect (str)
+(defun parse-rect (f str)
   "parse a rectacngle from a string like #218 @ 278,517: 18x19"
-  (plot (mapcar 'parse-integer (split (third (split-by-space (remove #\Colon str))) #\Comma))
+  (funcall f (mapcar 'parse-integer (split (third (split-by-space (remove #\Colon str))) #\Comma))
         (mapcar 'parse-integer (split (fourth (split-by-space str)) #\x))))
+
+(defun claimp (xy wh)
+  ;; test to see if all 1's
+  (let ((w (first wh))
+        (h (second wh)))
+    (every #'identity (loop for y
+                            from (second xy)
+                              to (+ (second xy) h -1)
+                            collect (let ((iy (* y *width*))
+                                          (x (first xy)))
+                                      (every #'(lambda (x) (= x 1)) (subseq *fabric* (+ x iy) (+ x iy w))))))))
+
+(defun find-claim (rects)
+  (loop for line in rects do
+       (if (parse-rect 'claimp line)
+           (format t "answer: ~a~%" line)
+           )
+       )
+  )
 
 (with-open-file (input "input.txt")
   (when input
     (loop for line = (read-line input nil)
-          while line do (parse-rect line))))
+          while line do (progn
+                          (parse-rect 'plot line)
+                          (push line *rects*)))))
 
-(let ((total (count 1 *data* :test '<)))
-  (format t "...done. ~a~%" total))
+(let ((total (count 1 *fabric* :test '<)))
+  (format t "...done. ~a inches of overlap~%" total))
+
+(find-claim *rects*)
