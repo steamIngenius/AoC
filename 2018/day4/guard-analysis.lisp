bf8ffc33e290694b0d7e2be127f2afde0182088e
micah <micahbhawkins@gmail.com>
2018-12-07 16:33:14 -0800

    close to finishing day 4 part 1

--- 2018/day4/guard-analysis.lisp
+++ 2018/day4/guard-analysis.lisp
@@ -1,5 +1,42 @@
 (defparameter *events* nil)
 (defparameter *wonky-time-offset* 500)
+(defparameter *guard-profiles* nil)
+
+(defun parse-guard-id (event)
+  (parse-integer (second (split (getf event :log) #\#)) :junk-allowed t))
+
+(defun is-shift-change? (event)
+  (search "shift" (getf event :log)))
+
+(defun asleep? (event)
+  (search "asleep" (getf event :log)))
+
+(defun new-guard (id)
+  (list :id id :sleep-patterns (make-array 60 :initial-element 0)))
+
+(defun update-guard (guard asleep awake)
+  ;; calc start end of sleep
+  ;; update the minutes of the guard's sleep patterns heatmap
+  )
+
+(defun process-sleep (guard-id asleep-at awake-at)
+  (let ((guard-profile (find guard-id
+                             *guard-profiles*
+                             :key (extract :id))))
+    (if guard-profile
+        (setf guard-profile (update-guard guard-profile asleep-at awake-at))
+        (setf *guard-profiles*
+              (cons (new-guard guard-id) *guard-profiles*)))))
+
+(defun analyze-event-logs (events)
+  (defvar current-guard)
+  (defvar asleep-at)
+  (loop for event in events do
+    (cond ((is-shift-change? event) (setf current-guard (parse-guard-id event)))
+          ((asleep? event) (setf asleep-at (getf event :timestamp)))
+          (t (process-sleep current-guard
+                            asleep-at
+                            (getf event :timestamp))))))
 
 (defun split (str chr)
   "Returns a list of substrings of str split by chr"
@@ -13,13 +50,14 @@
          (date-time (split raw #\Space))
          (date (first date-time))
          (time-part (second date-time))
+         (hour (parse-integer (first (split time-part #\Colon))))
          (min (parse-integer (second (split time-part #\Colon))))
          (day (parse-integer (third (split date #\-))))
          (month (parse-integer (second (split date #\-))))
          (year (parse-integer (first (split date #\-)))))
     (encode-universal-time 0
                            min
-                           0
+                           hour
                            day
                            month
                            ;; 1518 is too early for universal time lol
@@ -49,15 +87,17 @@
 
 (setf *events* (sort *events* #'< :key (extract :timestamp)))
 
+(analyze-event-logs *events*)
 
 ;; DONE
 ;; read lines and parse into a hash or plist
 ;; place in an assoc. seq. of events
 ;; order seq. of events by timestamp
-;;
-;; TODO
 ;; process events and create a data structure with:
 ;;   an entry for each guard
+;;
+;; TODO
+;; process events cont'd...
 ;;   a heatmap for each guard's sleeping patterns
 ;; ask 2 questions:
 ;;   which guard sleeps the most?
