ee7de6b5d3cbb9381ff69f3a75793141e21ccf1a
Micah Hawkins <micahbhawkins@gmail.com>
2018-12-08 08:43:14 -0800

    completes day 4

--- 2018/day4/guard-analysis.lisp
+++ 2018/day4/guard-analysis.lisp
@@ -15,9 +15,17 @@
   (list :id id :sleep-patterns (make-array 60 :initial-element 0)))
 
 (defun update-guard (guard asleep awake)
-  ;; calc start end of sleep
-  ;; update the minutes of the guard's sleep patterns heatmap
-  )
+  (let ((start (multiple-value-bind
+                     (second minute)
+                   (decode-universal-time asleep)
+                 minute))
+        (finish (multiple-value-bind
+                      (second minute)
+                    (decode-universal-time awake)
+                  minute)))
+    (loop for i from start to (1- finish) do
+      (incf (elt (getf guard :sleep-patterns) i))))
+  guard)
 
 (defun process-sleep (guard-id asleep-at awake-at)
   (let ((guard-profile (find guard-id
@@ -85,9 +93,35 @@
     (loop for line = (read-line input nil)
           while line do (injest line))))
 
+;; order events by timestamp & analyze
 (setf *events* (sort *events* #'< :key (extract :timestamp)))
-
 (analyze-event-logs *events*)
+
+;; order guards by sleepiest
+(setf *guard-profiles* (sort *guard-profiles* #'> :key #'(lambda (plist) (reduce #'+ (getf plist :sleep-patterns)))))
+
+(defparameter sleepy (getf (first *guard-profiles*) :id))
+(defparameter sleepy-time (position (reduce #'max
+                                            (getf (first *guard-profiles*) :sleep-patterns))
+                                    (getf (first *guard-profiles*) :sleep-patterns)))
+(format t "sleepiest guard: ~a~%" sleepy)
+(format t "he is most likely to be asleep at minute ~a~%" sleepy-time)
+(format t "the answer to part 1 is: ~a~%" (* sleepy sleepy-time))
+
+;; part 2
+(setf *guard-profiles* (sort *guard-profiles* #'> :key #'(lambda (plist)
+                                               (reduce #'max (getf plist :sleep-patterns)))))
+(let* ((guard (second *guard-profiles*))
+       (guard-id (getf guard :id))
+       (sleep-times (reduce #'max (getf guard :sleep-patterns)))
+       (max-minute (position sleep-times (getf guard :sleep-patterns))))
+  (format t "guard ~a fell asleep ~a times on minute ~a~%"
+          guard-id
+          sleep-times
+          max-minute)
+  (format t "the answer for part 2 is: ~a~%" (* guard-id max-minute)))
+
+
 
 ;; DONE
 ;; read lines and parse into a hash or plist
@@ -95,11 +129,14 @@
 ;; order seq. of events by timestamp
 ;; process events and create a data structure with:
 ;;   an entry for each guard
-;;
-;; TODO
-;; process events cont'd...
 ;;   a heatmap for each guard's sleeping patterns
-;; ask 2 questions:
+;; Part 1 - ask 2 questions:
 ;;   which guard sleeps the most?
 ;;   which minute is the guard most consistently asleep for?
-;; multiply the results
+;;   multiply the guard's id and the minute
+;; Part 2 - ask 2 questions:
+;;   which guard is most freq asleep the same minute?
+;;   which minute?
+;;   multiply the guard's id and the minute
+;;
+;; TODO
